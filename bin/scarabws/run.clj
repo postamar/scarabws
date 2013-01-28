fde01019fab0b973572b40e8c42e30d2ec9dd764
Marius Posta <mariusposta@gmail.com>
2013-01-28 15:16:28 -0500

    http port number fixed to 8050

--- bin/scarabws/run.clj
+++ bin/scarabws/run.clj
@@ -2,5 +2,4 @@
   (:use ring.adapter.jetty)
   (:require scarabws.web))
 
-(let [port (Integer/parseInt (get (System/getenv) "PORT" "8088"))]
-  (run-jetty #'scarabws.web/app {:port port}))
+(run-jetty #'scarabws.web/app {:port 8050})
