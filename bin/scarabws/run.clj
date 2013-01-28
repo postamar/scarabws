eef0aeab2f5353bfe624017e540eb9c03c2577d8
Marius Posta <mariusposta@gmail.com>
2013-01-28 16:29:59 -0500

    rm bin/

--- bin/scarabws/run.clj
+++
@@ -1,8 +0,0 @@
-(ns scarabws.run
-  (:use ring.adapter.jetty)
-  (:require scarabws.web)
-  (:gen-class))
-
-(defn -main [& args]
-  (scarabws.core/load-scarab)
-  (run-jetty #'scarabws.web/app {:port 8050}))
