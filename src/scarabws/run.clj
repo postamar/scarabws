2ac8a5c3b37e74038e4309ff21e9bb96f14020ef
Marius Posta <mariusposta@gmail.com>
2013-01-28 15:34:41 -0500

    forgot gen-class

--- src/scarabws/run.clj
+++ src/scarabws/run.clj
@@ -1,6 +1,7 @@
 (ns scarabws.run
   (:use ring.adapter.jetty)
-  (:require scarabws.web))
+  (:require scarabws.web)
+  (:gen-class))
 
 (defn -main [& args]
   (run-jetty #'scarabws.web/app {:port 8050}))
