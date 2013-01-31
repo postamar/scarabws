3dccb3c946537f4e16d869af2b7b5cbe39f01c8a
Marius Posta <mariusposta@gmail.com>
2013-01-30 20:56:58 -0500

    reduced memory load

--- src/scarabws/core.clj
+++ src/scarabws/core.clj
@@ -4,35 +4,36 @@
 
 (def letters [\a \b \c \d \e \f \g \h \i \j \k \l \m \n \o \p \q \r \s \t \u \v \w \x \y \z])
 
-(defn- match-in-trie [node indices jokers current]
-  (if (seq node)
-    (if (empty? indices)
-      (if (= current 26)
-        (node [])
-        (concat (match-in-trie node [] jokers (+ 1 current))
-                (mapcat #(match-in-trie (node (vector current (+ 1 %))) [] (- jokers 1 %) (+ 1 current)) (range 0 jokers))))        
-      (let [[[i n] & remaining] indices]
-        (if (= i current)
-          (concat (match-in-trie node remaining jokers (+ 1 i))
-                  (mapcat #(match-in-trie (node (vector i %)) remaining jokers (+ 1 i)) (range 1 (+ n 1)))
-                  (mapcat #(match-in-trie (node (vector i (+ n 1 %))) remaining (- jokers 1 %) (+ 1 i)) (range 0 jokers)))
-          (concat (match-in-trie node indices jokers (+ 1 current))
-                  (mapcat #(match-in-trie (node (vector current (+ 1 %))) indices (- jokers 1 %) (+ 1 current)) (range 0 jokers))))))))
-
-(defn- make-trie [level pairs]
-  (let [keys (map #(nth (first %) level []) pairs)]    
-    (into (sorted-map)
-          (map (fn [[key val]]
-                 [key (if (= [] key)                          
-                        (map last val)
-                        (make-trie (inc level) val))])
-               (apply merge-with conj (into {} (map vector (into #{} keys) (repeat []))) (map hash-map keys pairs))))))
-
 (defn- make-indexfn [indices]
   #(sort-by first compare (into [] (frequencies (map indices %)))))
 
+(defn- make-key-string [key]
+  (apply str (doall (mapcat #(repeat (last %) (letters (first %))) key))))
+  
+(defn- make-table [key level pairs]
+  (let [child-keys (map #(nth (first %) level []) pairs)
+        child-key-set (into #{} child-keys)
+        children (apply merge-with conj (zipmap child-key-set (repeat [])) (map hash-map child-keys pairs))
+        own (children [])]
+    (into {(make-key-string key) (if own (clojure.string/join "," (map last own)) "")}
+          (mapcat #(make-table (conj key %) (inc level) (children %))
+                  (filter seq child-key-set)))))
+
+(defn- match [table keymap node-key k jokers]
+  (when-let [own (table (make-key-string node-key))]
+    (apply concat
+           (into [] (if (seq own) 
+                      (clojure.string/split own #"\,")))
+           (for [i (range k 26)
+                 :let [maxval (+ jokers (or (keymap i) 0))]
+                 c (range 1 (inc maxval))]
+             (match table keymap
+                    (conj node-key [i c])
+                    (inc i)
+                    (min jokers (- maxval c)))))))
+  
 (defn- make-dictionnary [language contents]
-  (let [words (re-seq #"\w+" contents)
+  (let [words (clojure.string/split-lines contents)
         freqs (frequencies contents)
         weights (map #(or (freqs %) 0) letters)
         alist (reverse (sort-by last compare (map vector letters weights)))
@@ -43,7 +44,7 @@
      :lettercount (reduce + (map count words))
      :counts (map vector (map (comp str first) alist) (map last alist))
      :indices indices
-     :trie (make-trie 0 (map #(vector (indexfn %) %) words))}))
+     :table (into (sorted-map) (make-table [] 0 (map #(vector (indexfn %) %) words)))}))
 
 (def scarab (atom nil))
 
@@ -59,16 +60,16 @@
                          clj))
            (into {} (map (fn [[language txt-file-path]]
                            (let [clj-file-path (str path language ".clj")
-                                 dict (make-dictionnary language (clojure.string/lower-case (slurp txt-file-path)))]
+                                 dict (make-dictionnary language (slurp txt-file-path))]
                              (spit clj-file-path (pr-str dict))
                              [language dict]))
                          (filter (comp nil? clj first) 
                                  (map pop (filter #(= "txt" (peek %)) files))))))))
 
 (defn load-scarab [scarab-current-value path]
-  (if (not (nil? scarab-current-value))
-    scarab-current-value
-    (load-dictionnary-files path)))
+  (if (nil? scarab-current-value)
+    (load-dictionnary-files path)
+    scarab-current-value))
 
 (defn get-languages []
   (into #{} (map first (deref scarab))))
@@ -82,10 +83,14 @@
   (let [dict ((deref scarab) language)
         letters (clojure.string/replace word-query #"_" "")
         jokers (- (count word-query) (count letters))
-        all-matches (into [] (match-in-trie (dict :trie) ((make-indexfn (dict :indices)) letters) jokers 0))
+        all-matches (match (dict :table) 
+                           (into {} ((make-indexfn (dict :indices)) letters)) 
+                           []
+                           0
+                           (- (count word-query) (count letters)))
         query-pattern (re-pattern (clojure.string/replace word-query #"_" "[a-z]"))]
     (into {:language language
            :query word-query
            :exact-matches (filter #(re-matches query-pattern %) all-matches)}
-          (map #(vector (count (first %)) %)
-               (map sort (partition-by count (sort-by count compare (when all? all-matches))))))))
+          (map #(vector (count (first %)) (sort %)) 
+               (partition-by count (sort-by count compare (when all? all-matches)))))))
